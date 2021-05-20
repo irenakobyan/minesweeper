d66f0d2ce0b21d9147cf6cb0d02bd6c44c26a4b9
Irena <irena_hakobyan@edu.aua.am>
2021-05-20 10:44:58 +0400

    update

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -6,6 +6,11 @@ const columns = [
   { field: 'firstName', headerName: 'First name', width: 130 },
   { field: 'lastName', headerName: 'Last name', width: 130 },
   {
+    field: 'userName',
+    headerName: 'userName',
+    width: 130,
+  },
+  {
     field: 'age',
     headerName: 'Age',
     type: 'number',
@@ -23,15 +28,15 @@ const columns = [
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, lastName: 'Snow', firstName: 'Jon', userName: 'Snow', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', userName: 'Snow', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', userName: 'Snow', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', userName: 'Snow', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', userName: 'Snow', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, userName: 'Snow', age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', userName: 'Snow', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', userName: 'Snow', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', userName: 'Snow', age: 65 },
 ];
 
 export default function Dashboard() {
